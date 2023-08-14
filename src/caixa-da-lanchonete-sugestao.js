aaf693d83405a61af6f43673813b3f6346a23637
Matheus Diogo <matheusdiogoeng@gmail.com>
2023-08-14 20:46:39 -0300

    Adicionando sugestão e arrumando saídas esperadas

--- src/caixa-da-lanchonete-sugestao.js
+++ src/caixa-da-lanchonete-sugestao.js
@@ -7,7 +7,7 @@ class CaixaDaLanchonete {
     calcularValorDaCompra(metodoDePagamento, itens) {
 
         if(itens.length === 0){
-            return "Não há itens no carrinho de compra!";
+            return console.log("Não há itens no carrinho de compra!");
         }
 
         const pedido = cardapio.verificar_itens(itens.map(item => item.split(',')[0]));
@@ -20,9 +20,11 @@ class CaixaDaLanchonete {
 
             let invalido = 1;
             if (quantidade === '0') {
-                return 'Quantidade inválida!';
+                console.log(nomeItem + ': Quantidade inválida!');
+                invalido = 0;
             }else if (pedido[nomeItem]) {
                 const valorItem = cardapio.itens[nomeItem].valor * quantidade;
+                console.log(nomeItem + ' (' + quantidade + 'x): R$ ' + valorItem.toFixed(2).replace('.', ','));
                 ValorTotal += valorItem;
                 invalido = 0;
             } else {
@@ -32,27 +34,28 @@ class CaixaDaLanchonete {
                         invalido = 0;
                         if (principalNoPedido && parseInt(pratos[principal][0])) {
                             const valorExtra = cardapio.itens[principal].extras[nomeItem].valor * quantidade;
+                            console.log(nomeItem + ' (' + quantidade + 'x): R$ ' + valorExtra.toFixed(2).replace('.', ','));
                             ValorTotal += valorExtra;
                             break;
                         } else {
-                            return "Item extra não pode ser pedido sem o principal";
+                            console.log(nomeItem + ": Item extra não pode ser pedido sem o principal");
                         }
                     }
                 }
             }
             if (invalido){
-                return "Item inválido!";
+                console.log(nomeItem + ": Item inválido!");
             }
         });
         
         if(metodoDePagamento === 'dinheiro'){
             ValorTotal -= ValorTotal*0.05;
-            return 'R$ ' + ValorTotal.toFixed(2).replace('.',',');
+            return console.log('Total: R$ ' + ValorTotal.toFixed(2).replace('.',','));
         }else if(metodoDePagamento === 'credito'){
             ValorTotal += ValorTotal*0.03;
-            return 'R$ ' + ValorTotal.toFixed(2).replace('.',',');
+            return console.log('Total: R$ ' + ValorTotal.toFixed(2).replace('.',','));
         }else{
-            return 'R$ ' + ValorTotal.toFixed(2).replace('.',',');
+            return console.log('Total: R$ ' + ValorTotal.toFixed(2).replace('.',','));
         }
     }
 }
