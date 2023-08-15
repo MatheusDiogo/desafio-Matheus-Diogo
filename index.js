4c3ad7afd323506901ecefdece7ed32a4cf37ddc
Matheus Diogo <matheusdiogoeng@gmail.com>
2023-08-14 23:59:11 -0300

    Excluindo arquivos para testes. Projeto validado!

--- index.js
+++
@@ -1,7 +0,0 @@
-import {CaixaDaLanchonete} from "./src/caixa-da-lanchonete.js";
-
-const pedido = ['cafe,1', 'pizza,1'];
-
-const resultado = new CaixaDaLanchonete().calcularValorDaCompra('dinheiro', pedido);
-
-console.log(resultado)
